fbc38e4564e38f41829aec9f7afc2d1275807a84
shubham05tawade <shubham05tawade@gmail.com>
2024-04-28 14:24:55 +0100

    Update index.go

--- api/index.go
+++ api/index.go
@@ -1,28 +1,10 @@
-package main
-
+package handler
+ 
 import (
-	"encoding/json"
-	"log"
-	"net/http"
+  "fmt"
+  "net/http"
 )
-
-type response struct {
-	Response string `json:"response"`
-}
-
-func getHelloWorld(w http.ResponseWriter, r *http.Request) {
-	var response = response{
-		Response: "Hello World",
-	}
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(response)
-}
-
-func restApi() {
-	http.Handle("/hello", http.HandlerFunc(getHelloWorld))
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-func main() {
-	restApi()
+ 
+func Handler(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
 }
