872a49a4fde1239958a1e5f2ddc07d6952b4616e
shubham05tawade <shubham05tawade@gmail.com>
2024-04-28 15:07:11 +0100

    Update index.go

--- api/index.go
+++ api/index.go
@@ -10,15 +10,10 @@ type response struct {
 	Response string `json:"response"`
 }
 
-func getHelloWorld(w http.ResponseWriter, r *http.Request) {
+func hello(w http.ResponseWriter, r *http.Request) {
 	var response = response{
 		Response: "Hello World",
 	}
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(response)
-}
-
-func Handler() {
-	http.Handle("/hello", http.HandlerFunc(getHelloWorld))
-	log.Fatal(http.ListenAndServe(":8080", nil))
 }
